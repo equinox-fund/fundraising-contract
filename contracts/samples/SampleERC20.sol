970fb17413eb1360c0f799d803ddcd69d7cfce3a
jeromevvb <jeromevvb@gmail.com>
2022-06-23 10:54:10 +0200

    Simple safe version of ERC20 Sample

--- contracts/samples/SampleERC20.sol
+++ contracts/samples/SampleERC20.sol
@@ -2,38 +2,23 @@
 pragma solidity 0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SampleERC20 is Context, AccessControl, ERC20 {
+contract SampleERC20 is ERC20, Ownable {
     uint8 public newDecimals;
-
-    bytes32 public constant WITHDRAWER_ROLE = keccak256("WITHDRAWER_ROLE");
 
     constructor(uint8 _decimals) ERC20("SampleERC20", "ERC") {
         newDecimals = _decimals;
     }
 
     function mintToWallet(address _address, uint256 _amount)
-        public
-        payable
-        returns (bool)
+        external
+        onlyOwner
     {
         _mint(_address, _amount);
-        return true;
     }
 
     function decimals() public view virtual override returns (uint8) {
         return newDecimals;
-    }
-
-    // just sample funciton use SafeERC20
-    function withdraw(
-        IERC20 token,
-        address recipient,
-        uint256 amount
-    ) external {
-        require(hasRole(WITHDRAWER_ROLE, _msgSender()), "wrong role");
-        require(token.transfer(recipient, amount), "transaction failed");
     }
 }
